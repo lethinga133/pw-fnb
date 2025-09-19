d61eb38f6ad1d8ab0e9943ee1495b83a44d277b9
Nga Le <nga.lt@gmail.com>
2025-09-19 08:26:04 +0700

    Lessson-03

--- tests/lesson-03/array-util.js
+++ tests/lesson-03/array-util.js
@@ -15,52 +15,53 @@ console.log(newNum3);
 // ● Khái niệm: Phương thức reduce() áp dụng một hàm lên từng phần tử của mảng (từ
 // trái qua phải) để trả về một giá trị duy nhất.
 
-let numbers = [1, 2, 3, 4];
-let sum = numbers.reduce((total, num) => total + num, 0);
+let numbers3 = [1, 2, 3, 4];
+let sum = numbers3.reduce((total, num) => total + num, 0);
 console.log(sum); // Kết quả: 10
 //total: biến nhận giá trị duy nhất
 //num: phần tử của mảng
 //0 giá trị khởi tạo cho biến total
 
-// some()
-// ● Khái niệm: Phương thức some() kiểm tra xem có ít nhất một phần tử trong mảng thỏa
+// some() 
+// kiểm tra xem có ít nhất một phần tử trong mảng thỏa
 // mãn điều kiện được chỉ định trong hàm callback. Trả về true nếu tìm thấy, ngược lại trả
 // về false.
-// Ví dụ:
 
-let numbers = [1, 2, 3, 4];
-let hasEven = numbers.some(num => num % 2 === 0);
+let numbers4 = [1, 2, 3, 4];
+let hasEven = numbers4.some(num => num % 2 === 0);
 console.log(hasEven); // Kết quả: true
+
 // 6. every()
 // ● Khái niệm: Phương thức every() kiểm tra xem tất cả các phần tử trong mảng có thỏa
 // mãn điều kiện được chỉ định trong hàm callback hay không. Trả về true nếu tất cả đều
 // thỏa mãn, ngược lại trả về false.
-// Ví dụ:
-let numbers = [2, 4, 6, 8];
-let allEven = numbers.every(num => num % 2 === 0);
+
+let numbers5 = [2, 4, 6, 8];
+let allEven = numbers5.every(num => num % 2 === 0);
 console.log(allEven); // Kết quả: true
 
 // push() thêm một hoặc nhiều phần tử vào cuối mảng và
 // trả về độ dài mới của mảng.
-let numbers = [1, 2, 3];
+let numbers6 = [1, 2, 3];
 numbers.push(4, 5);
-console.log(numbers); // Kết quả: [1, 2, 3, 4, 5]
+console.log(numbers6); // Kết quả: [1, 2, 3, 4, 5]
 
 // shift()
 // ● Khái niệm: Phương thức shift() loại bỏ phần tử đầu tiên của mảng và trả về phần
 // tử bị loại bỏ. Phương thức này thay đổi độ dài của mảng**.**
 
-let numbers = [1, 2, 3];
-let firstElement = numbers.shift();
+let numbers7 = [1, 2, 3];
+let firstElement = numbers7.shift();
 console.log(firstElement); // Kết quả: 1
-console.log(numbers); // Kết quả: [2, 3]
+console.log(numbers7); // Kết quả: [2, 3]
+
 // 9. sort()
-// ● Khái niệm: Phương thức sort() sắp xếp các phần tử của mảng theo thứ tự tăng
+// ● sắp xếp các phần tử của mảng theo thứ tự tăng
 // dần hoặc theo hàm so sánh được cung cấp. Nó thay đổi mảng ban đầu.
 // ● sort() mặc định: sắp xếp các phần tử của mảng như chuỗi theo thứ tự từ điển
 // học Unicode (ASCII hoặc UTF-16). Điều này có nghĩa là mỗi phần tử sẽ được chuyển
 // đổi thành chuỗi (nếu không phải là chuỗi) trước khi so sánh.
-// Ví dụ:
-let numbers = [4, 2, 3, 1];
+
+let numbers8 = [4, 2, 3, 1];
 numbers.sort();
-console.log(numbers);
+console.log(numbers8);
